871d70d660417c18dc3eda6e23dbf67e42b3c363
yanitski <yanitski@ph1.uni-koeln.de>
2024-12-11 17:34:45 -0700

    new TextNode class correctly printing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world")
+#print("hello world")
+from textnode import TextNode, TextType
+
+def main() -> None:
+    test: TextNode = TextNode("This is a test text node", TextType.ITALIC, "https://www.boot.dev")
+    print(repr(test))
+    return
+
+
+main()
