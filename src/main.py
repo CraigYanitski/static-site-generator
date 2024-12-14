27cd912aa02e722299ab7114b13b729377f4d5e4
yanitski <yanitski@ph1.uni-koeln.de>
2024-12-14 16:37:19 -0700

    new function to copy static files to the public dir
    * runs as main function - accessible with main.sh

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
-#print("hello world")
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
-def main() -> None:
-    test: TextNode = TextNode("This is a test text node", TextType.ITALIC, "https://www.boot.dev")
-    print(repr(test))
+def main() -> int:
+    cp_dir("static", "public")
+    return 0
+
+def cp_dir(original: str, dest: str) -> None:
+    # Find current files in destination directory and remove them
+    if os.path.exists(dest):
+        old_files: list = os.listdir(dest)
+        for filename in old_files:
+            if os.path.isfile(os.path.join(dest, filename)):
+                os.remove(os.path.join(dest, filename))
+            else:
+                shutil.rmtree(os.path.join(dest, filename))
+    # Find files in original directory and copy them to destination
+    static_files: list = os.listdir(original)
+    for filename in static_files:
+        o_file: str = os.path.join(original, filename)
+        d_file: str = os.path.join(dest, filename)
+        if os.path.isfile(o_file):
+            shutil.copy(o_file, d_file)
+        else:
+            os.mkdir(d_file)
+            # recurse as needed
+            cp_dir(o_file, d_file)
     return
 
 
